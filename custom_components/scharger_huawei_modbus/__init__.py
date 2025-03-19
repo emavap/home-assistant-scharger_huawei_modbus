8f4686e2cf94e8782a8e63bb8b396e8aaf40765c
Marco Camilli <marco@emavap.be>
2025-03-19 21:39:54 +0100

    Add files via upload

--- custom_components/scharger_huawei_modbus/__init__.py
+++ custom_components/scharger_huawei_modbus/__init__.py
@@ -3,9 +3,7 @@ from .modbus_server import start_modbus_server, RegisterManager
 
 
 async def async_setup_entry(hass, config_entry):
-    port = config_entry.data.get("port", 8502)
-    if config_entry.options:
-        port = config_entry.options.get("port", port)
+    port = config_entry.options.get("port", 8502)
 
     register_manager = RegisterManager(hass)
     start_modbus_server(register_manager, port=port)
--- custom_components/scharger_huawei_modbus/config_flow.py
+++ custom_components/scharger_huawei_modbus/config_flow.py
@@ -3,6 +3,19 @@ import voluptuous as vol
 from .const import DOMAIN
 
 
+class HuaweiChargerFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Huawei Charger", data={})
+
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
+
+    async def async_get_options_flow(self, config_entry):
+        return HuaweiChargerOptionsFlowHandler(config_entry)
+
+
 class HuaweiChargerOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
@@ -15,25 +28,6 @@ class HuaweiChargerOptionsFlowHandler(config_entries.OptionsFlow):
             step_id="init",
             data_schema=vol.Schema({
                 vol.Optional("debug", default=self.config_entry.options.get("debug", False)): bool,
-                vol.Optional("port", default=self.config_entry.options.get("port", 8502)): int
+                vol.Optional("port", default=self.config_entry.options.get("port", 502)): int
             })
         )
-
-
-class HuaweiChargerFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="Huawei Charger", data=user_input)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Optional("debug", default=False): bool,
-                vol.Optional("port", default=8502): int
-            })
-        )
-
-    async def async_get_options_flow(self, config_entry):
-        return HuaweiChargerOptionsFlowHandler(config_entry)
