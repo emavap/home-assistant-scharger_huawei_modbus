a6569f1160a080df00d506ee76bd4781c443ade9
Marco Camilli <marco@emavap.be>
2025-03-19 23:12:03 +0100

    .

--- custom_components/scharger_huawei_modbus/config_flow.py
+++ custom_components/scharger_huawei_modbus/config_flow.py
@@ -1,34 +1,22 @@
 from homeassistant import config_entries
-import voluptuous as vol
 from .const import DOMAIN
 
-
-class HuaweiChargerFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class HuaweiChargerFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Huawei Charger", data={})
+            return self.async_create_entry(title="Huawei Charger", data=user_input)
 
-        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
+        return self.async_show_form(step_id="user", data_schema=None)
 
     @staticmethod
     def async_get_options_flow(config_entry):
         return HuaweiChargerOptionsFlowHandler(config_entry)
-
 
 class HuaweiChargerOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Optional("debug", default=self.config_entry.options["debug"] if "debug" in self.config_entry.options else False): bool,
-                vol.Optional("port", default=self.config_entry.options["port"] if "port" in self.config_entry.options else 502): int
-            })
-        )
+        return self.async_show_form(step_id="init", data_schema=None)
